a49456309d76a157b6e91dcfa4643daa0c3eb372
Santos Gonzalez <sgonzo3@gmail.com>
2019-12-05 22:42:29 -0500

    feat: Break and Session components function

--- src/App.js
+++ src/App.js
@@ -1,43 +1,126 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import AppClock from './components/AppClock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 class="app-title">
+        <h1 className="app-title">
           Pomodoro Clock
         </h1>
       </header>
-      <main>
-        <Break/>
-        <Session/>
-        <Timer/>
-      </main>
+      <Pomodoro />
       <footer>
       </footer>
     </div>
   );
 }
+class Pomodoro extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       break: 5,
+       session: 25,
+       active: false,
+       start: 0,
+       end: 0,
+       timer: 0,
+     };
+   }
+   decrementTime = (element, elementName) => {
+     if(element > 1) {
+       this.setState((prevState, props)=> {
+         return {
+           elementName: --prevState[elementName],
+         }
+       });
+     }
+   }
+   incrementTime = (element, elementName) => {
+       this.setState((prevState, props) => {
+         return {
+           elementName: ++prevState[elementName],
+         }
+       });
+   }
+  playPause = () => {
+     this.setState({
+       active: !this.state.active,
+       start: Date.now(),
+       end: (this.state.session * 60000) + Date.now(),
+       timer: (this.state.end - Date.now()) / 60000
+     })
+     this.update();
+  }
+  update = () => {
+    let incomplete = (this.state.end - Date.now());
 
+    if(!incomplete){
+      return;
+    }
+    this.setState({
+      timer: Math.floor(incomplete/1000),
+    });
+    window.requestAnimationFrame(this.update);
+  };
+
+  reset = () => {
+
+  }
+
+   render(){
+     return(
+       <main>
+         <Break
+           break={this.state.break}
+           decrementTime={this.decrementTime}
+           incrementTime={this.incrementTime}
+           />
+         <Session
+           session={this.state.session}
+           decrementTime={this.decrementTime}
+           incrementTime={this.incrementTime}
+           />
+         <Timer
+           break={this.state.break}
+           session={this.state.session}
+           active={this.state.active}
+           then={this.props.then}
+           playPause={this.playPause}
+           timer={this.state.timer}
+           update={this.update}
+           />
+       </main>
+     );
+   }
+}
 class Break extends React.Component {
    constructor(props) {
      super(props);
      this.state = {
-       value: 5,
+
      };
+   }
+   handleClick = (e) => {
+     e.target.innerText === '-'
+      ? this.props.decrementTime(this.props.break, 'break')
+      : this.props.incrementTime(this.props.break, 'break')
    }
    render(){
      return(
        <div>
          <label id="break-label">
-           <span>"Break Length"</span>
+           <h2>Break Length</h2>
          </label>
-         <button id="break-decrement"></button>
-         <span id="break-length">{this.state.value}</span>
-         <button id="break-increment"></button>
+         <button
+           id="break-decrement"
+           onClick={this.handleClick}
+           >-</button>
+         <span id="break-length">{this.props.break}</span>
+         <button
+           id="break-increment"
+           onClick={this.handleClick}
+           >+</button>
        </div>
      );
    }
